9972e7da37230cf68e96a6ecee3c14291bd75abd
gordiancrew <gordiancrew@gmail.com>
2020-12-02 19:16:11 +0300

    StartingTasks

---
+++ src/com/company/LinearProgramsTask1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class LinearProgramsTask1 {
+
+double a= 2.20;
+double b =3;
+double c=2;
+double z;
+void linearProgramsTask1() {
+    z=((a-3)*b/2)+c;
+    System.out.println(z);
+}
+
+}
