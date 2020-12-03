fc4e13d4e5af789023716df0f597087a35a0036e
gordiancrew <gordiancrew@gmail.com>
2020-12-04 01:13:25 +0300

    BranchingTask

---
+++ src/com/company/BranchinTask5.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class BranchinTask5 {
+    double x=5;
+    void branchinTask5(){
+        double f=x<=3? x*x-3*x+9:1/(Math.pow(x,3)+6);
+        System.out.println(f);
+
+
+    }
+
+
+}
